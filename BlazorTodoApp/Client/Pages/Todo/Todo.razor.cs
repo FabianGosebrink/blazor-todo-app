53dce78f09a1dd4863d7a3a97c21ff082d83dcb5
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2020-05-31 16:48:08 +0200

    added viewmodel file

---
+++ BlazorTodoApp/Client/Pages/Todo/Todo.razor.cs
@@ -0,0 +1,39 @@
+﻿using BlazorTodoApp.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTodoApp.Client.Pages.Todo
+{
+    public partial  class Todo : ComponentBase
+    {
+        protected override async Task OnInitializedAsync()
+        {
+            todoModels = await todoService.GetTodos();
+        }
+
+        public List<TodoDto> todoModels = new List<TodoDto>();
+
+        private async Task AddTodo(TodoDto todoModel)
+        {
+            var newModel = await todoService.AddTodo(todoModel);
+            todoModels.Insert(0, newModel);
+        }
+
+        private async Task UpdateTodo(TodoDto todoModel)
+        {
+            var updatedModel = await todoService.UpdateTodo(todoModel);
+            var existingModel = todoModels.First(x => x.Id == updatedModel.Id);
+            var index = todoModels.IndexOf(existingModel);
+
+            if (index != -1)
+            {
+                todoModels[index] = updatedModel;
+            }
+
+            todoModels = todoModels.OrderBy(x => x.Done).ToList();
+        }
+    }
+}
